94d73b34984d7ac0dfb7216a58e853c1983dd5b2
jlguijarro <joseluisguijarrorubio@gmail.com>
2021-07-22 21:24:12 +0200

    Added NPM scripts
    -NPM scripts added
    -package.json updated

---
+++ js/index-script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
+
+const reserveTable = () => {
+    var myModal = new bootstrap.Modal(document.getElementById('reserveModal'));
+    myModal.show();
+}
+
+const logIn = () => {
+    var myModal = new bootstrap.Modal(document.getElementById('loginModal'));
+    myModal.show();
+}
